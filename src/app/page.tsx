dcc3a281ca5b745f6e79379987dfcf9744ed5acf
fouxan <fouzanfouzan1310@gmail.com>
2024-10-30 20:16:23 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="underline text-center text-red-600 font-semibold">
+      Hello World
+      <Button>Click me</Button>
+    </div>
+  );
+}
